fbb373af5bde7c5e70e638d384944ef0123d7d93
James <jamestedesco802@gmail.com>
2020-05-12 17:03:32 -0400

    Seperated functions into two js files

--- public/app.js
+++ public/app.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", async (event) => {
 
-    // window.alert('Welcome! Deal yourself a card!')
-    // console.log('proof')
 
     // grab card from database
     listenToJudgeCard()
@@ -189,78 +187,8 @@ async function listenToJudgeCard() {
 }
 
 
-
-
-
-// button click: actively retrieves new card
-async function updateJudgeCard() {
-
-
-    // open database
-    const db = firebase.firestore();
-
-    // before we enter a loop, make sure there's at least one new card
-    // make sure cards are available
-    let incrementorPath = db.collection('incrementors').doc('judgeCardsIncrementor')
-    let judgeCards = db.collection('judgeCards');
-    await cardsRemaining(incrementorPath, judgeCards)
-
-
-
-
-    // on click, get new judge card --> write document ID to incrementor list so we can read that to everyone
-    let dbCards = await judgeCards.where('isUsed', '==', false).limit(1).get()
-    // start loop to access document
-    dbCards.forEach(card => {
-
-
-
-
-        // copy card's content to the single doc which transiently holds this active content
-        let judgeCardContent = card.data().content
-        judgeCards.doc('currentJudgeCard').set({
-            active_judgeCard: judgeCardContent
-        })
-        console.log('New judge card assigned in Firestore.')
-
-
-        // increment current active judge card counter
-        let number = 1
-        let incrementorLocation = db.collection('incrementors').doc('judgeCardsIncrementor')
-        changeCardCount(number, incrementorLocation)
-
-
-        // mark used to prevent repeat access
-        judgeCards.doc(card.id).update({
-            isUsed: true
-        })
-
-
-
-    })
-}
-
-// user is judge --> click event
-function userIsJudge() {
-
-    showSubmissions()
-
-}
-
-// view change
-function showSubmissions() {
-
-    console.log('View change: submitted cards')
-    // show submissions
-    document.getElementById('submitted-cards-wrapper').classList.add('reveal-element')
-    // hide personal cards
-    document.getElementById('local-cards-wrapper').classList.add('hide-element')
-}
-
-
-// view change
+// view change --> called by server listener
 function showPlayerCards() {
-
     console.log('View change: showing player cards now')
     document.getElementById('submitted-cards-wrapper').classList.remove('reveal-element')
     document.getElementById('local-cards-wrapper').classList.remove('hide-element')
@@ -284,9 +212,6 @@ async function cardsRemaining(incrementorPath, cardCollectionPath) {
     if (currentlyUsed >= totalDocs) {
         await recycleAllCards(incrementorPath, cardCollectionPath)
     }
-
-
-
 }
 
 
@@ -303,18 +228,7 @@ function changeCardCount(number, incrementorLocation) {
 }
 
 
-function devTools() {
 
-    // handle recycle button
-    let recycleBtn = document.getElementById('recycle-btn')
-    if (recycleBtn.style.display == 'none') {
-        recycleBtn.style.display = 'block'
-    } else {
-        recycleBtn.style.display = 'none'
-    }
-
-
-}
 
 
 // send local value out to server
@@ -424,32 +338,6 @@ function renderSubmission(submittedContentFromServer) {
 
 
 
-// called by submit button click
-async function submitThisCard(submitButton) {
-
-    // get submission text
-    let cardContent = submitButton.previousElementSibling.innerText
-
-
-    // send this submission to the server
-    const db = firebase.firestore();
-    let submittedCollection = db.collection('submittedCards')
-
-    submittedCollection.add({
-        submitted_content: cardContent,
-        timestamp: new Date().getTime()
-    })
-
-    // after submitting, delete the card
-    deleteCard(submitButton)
-
-
-    // move to submissions view - on submit click
-    showSubmissions()
-
-
-
-}
 
 // delete all player submissions
 async function deletePlayerSubmissions(db) {
